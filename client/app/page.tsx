f0ddd2c0b9e2719f66cc9febe993ab76af606283
witchbl4des <menzo.smit-adam@epitech.eu>
2024-07-14 21:30:01 +0200

    feat: Add DashboardCard component to Home page layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import DashboardCard from "@/components/common/dash_card";
+import { ServerIcon, User, WifiHighIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <>
     <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-      <DashboardCard />
+      <DashboardCard title="Switch" count={0} icon={<ServerIcon className="text-slate-500" size={72} />} />
+      <DashboardCard title="Vlan" count={0} icon={<WifiHighIcon className="text-slate-500" size={72} />} />
+      <DashboardCard title="User" count={0} icon={<User className="text-slate-500" size={72} />} />
+
     </div>
     </>
   );
