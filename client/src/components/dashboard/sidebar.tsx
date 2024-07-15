bc03db6b78df4210154c68068250b1645dc198b4
witchbl4des <menzo.smit-adam@epitech.eu>
2024-07-15 14:38:30 +0200

    feat: Add lazy loading for images and update dependencies

--- client/src/components/dashboard/sidebar.tsx
+++ client/src/components/dashboard/sidebar.tsx
@@ -1,60 +1,80 @@
-import {
-    Command,
-    CommandDialog,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-    CommandShortcut,
-  } from "@/components/ui/command";
-  import { Folders, LayoutDashboard, Newspaper, ScreenShareIcon, Settings, User } from "lucide-react";
-  import Link from "next/link";
-  
-  const Sidebar = () => {
-    return (
-      <div className="fixed top-0 left-0 h-full w-64 bg-secondary">
-        <Command className="h-full bg-secondary rounded-none">
-          <CommandInput placeholder="Type a command or search..." />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Suggestions">
-              <CommandItem>
-                <LayoutDashboard className="mr-2 h-4 w-4" />
-                <Link href="/">Dashboard</Link>
-              </CommandItem>
-              <CommandItem>
-                <Newspaper className="mr-2 h-4 w-4" />
-                <Link href="/news">Logs</Link>
-              </CommandItem>
-              <CommandItem>
-                <Folders className="mr-2 h-4 w-4" />
-                <Link href="#">Monitor</Link>
-              </CommandItem>
-              <CommandItem>
-                <ScreenShareIcon className="mr-2 h-4 w-4" />
-                <Link href="#">NetXMS</Link>
-              </CommandItem>
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup heading="Settings">
-              <CommandItem>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-                <CommandShortcut>⌘ + p</CommandShortcut>
-              </CommandItem>
-              <CommandItem>
-                <Settings className="mr-2 h-4 w-4" />
-                <span>Settings</span>
-                <CommandShortcut>⌘ + ,</CommandShortcut>
-              </CommandItem>
-            </CommandGroup>
-          </CommandList>
-        </Command>
+import { Command, CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator, CommandShortcut } from "@/components/ui/command";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Folders, LayoutDashboard, Newspaper, ScreenShareIcon, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import logo from "../img/igy.png";
+import Image from "next/image";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-full w-64 bg-secondary flex flex-col">
+      <div className="flex items-center justify-between p-4">
+        <Link href="/">
+          <Image src={logo} alt="logo" width={40} height={40} />
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarImage src="https://github.com/witchbl4dess" alt="@shadcn" />
+              <AvatarFallback className="text-black">MS</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">
+                Profile
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">
+                Logout
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-    );
-  };
-  
-  export default Sidebar;
-  
+      <Command className="flex-grow bg-secondary rounded-none">
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              <Link href="/">Dashboard</Link>
+            </CommandItem>
+            <CommandItem>
+              <Newspaper className="mr-2 h-4 w-4" />
+              <Link href="/news">Logs</Link>
+            </CommandItem>
+            <CommandItem>
+              <Folders className="mr-2 h-4 w-4" />
+              <Link href="#">Monitor</Link>
+            </CommandItem>
+            <CommandItem>
+              <ScreenShareIcon className="mr-2 h-4 w-4" />
+              <Link href="#">NetXMS</Link>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘ + p</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘ + ,</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </div>
+  );
+};
+
+export default Sidebar;
